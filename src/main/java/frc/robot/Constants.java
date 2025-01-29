681f488e06c8333d6b6cc28046c85639e6c2c8ce
mando8119 <armando.g.barron1@gmail.com>
2025-01-28 22:59:52 -0800

    Fresh bools

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,8 +16,8 @@ public final class Constants {
   public static class RobotConstants{}
   public static class OperatorConstants {
 
-    public static final boolean isTankOperation = false;
-    public static final boolean isArcadeOperation = true;
+    public static final boolean isTankOperation = true;
+    public static final boolean isArcadeOperation = false;
 
     public static final int kDriverControllerPort = 0;
     public static final int XBOX_Left_Y = 0;
@@ -31,7 +31,7 @@ public final class Constants {
     public static final int leftMotor2 = 3;
 
     public static final boolean leftReversed = false;
-    public static final boolean rightReversed = true;
+    public static final boolean rightReversed = false;
 
     public static final double speed = .5;
   }
